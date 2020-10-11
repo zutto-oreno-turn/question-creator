73095d348f26a1d0afa01c916cd99f93a33db3c6
kotaro36 <yamamoto.kintaro@gmail.com>
2020-10-11 16:58:02 +0900

    Add CRLF -> empty

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ let params = {
   count: 10
 };
 if (previous != null) {
-  params.since_id = previous.questions[0].id;
+  params.since_id = previous.questions.slice(-1)[0].id;
   console.log(JSON.stringify(params));
 }
 
@@ -28,11 +28,26 @@ const client = new twitter({
 });
 
 const formatText = text => {
-  const words = text.split(' ');
+  let formated = text;
+
+  const ranges = [
+    '\ud83c[\udf00-\udfff]',
+    '\ud83d[\udc00-\ude4f]',
+    '\ud83d[\ude80-\udeff]',
+    '\ud7c9[\ude00-\udeff]',
+    '[\u2600-\u27BF]'
+  ];
+  const regexp = new RegExp(ranges.join('|'), 'g');
+  formated = formated.replace(regexp, '');
+  formated = formated.replace(/\n/g, '');
+  formated = formated.trim();
+
+  const words = formated.split(' ');
   if (words.length == 1) {
     return '';
   }
-  const formated = words.filter((word, index) => {
+
+  const excluded = words.filter((word, index) => {
     if (word.substring(0, 1) === '#') {
       return false;
     }
@@ -41,7 +56,7 @@ const formatText = text => {
     }
     return true;
   });
-  return formated.join(' ');
+  return excluded.join(' ').trim();
 };
 
 const outputTweets = (error, tweets) => {
@@ -54,20 +69,25 @@ const outputTweets = (error, tweets) => {
   let outputs = [];
   tweets.forEach(tweet => {
     const text = formatText(tweet.full_text);
-    if (text.length > 0) {
-      const output = {
-        id: tweet.id,
-        profile: {
-          name: tweet.user.name,
-          image: tweet.user.profile_image_url_https.replace('_normal', '_400x400')
-        },
-        sentence: text
-      };
-      outputs.push(output);
+    if (text.length == 0) {
+      return;
     }
+    if (tweet.id == params.since_id) {
+      return;
+    }
+    const output = {
+      id: tweet.id,
+      profile: {
+        name: tweet.user.name,
+        image: tweet.user.profile_image_url_https.replace('_normal', '_400x400')
+      },
+      sentence: text
+    };
+    outputs.push(output);
   });
 
   console.log('add tweets: ' + outputs.length);
+  outputs = outputs.reverse();
 
   if (previous != null) {
     outputs = previous.questions.concat(outputs);
